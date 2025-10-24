d3ebc0188d1fd06461a055dfc65f3e4b71495b11
Gal Jeza <gal.jeza55@gmail.com>
2025-10-24 11:55:25 +0200

    .

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,26 @@
 return {
   {
     "rebelot/kanagawa.nvim",
-    config = function()
-      vim.cmd("colorscheme kanagawa-dragon")
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      dimInactive = false,
+      theme = "wave",
+      background = { dark = "wave", light = "lotus" },
+      colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
+    },
+    config = function(_, opts)
+      require("kanagawa").setup(opts)
+      vim.cmd.colorscheme("kanagawa-wave")
     end,
+  },
+
+  -- tell LazyVim to use this colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-wave",
+    },
   },
 }
