864c52bc6e8e7d34e390cae55edf0064194b8d4b
Gal Jeza <gal.jeza55@gmail.com>
2026-01-07 14:30:04 +0100

    .

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,78 @@
 return {
   {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = function()
+        require("catppuccin").load()
+      end,
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1001,
+    opts = {
+      no_italic = true,
+      term_colors = true,
+      transparent_background = false,
+      styles = {
+        comments = {},
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+      },
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+      integrations = {
+        telescope = {
+          enabled = true,
+          style = "nvchad",
+        },
+        dropbar = {
+          enabled = true,
+          color_mode = true,
+        },
+      },
+    },
+
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
+  {
     "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,
     opts = {
-      transparent = true,
+      transparent = false,
       dimInactive = false,
-      theme = "wave",
-      background = { dark = "wave", light = "lotus" },
-      colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
+      theme = "dragon",
+      background = { dark = "dragon", light = "lotus" },
     },
-    config = function(_, opts)
-      require("kanagawa").setup(opts)
-      vim.cmd.colorscheme("kanagawa-wave")
-    end,
+    -- config = function(_, opts)
+    --   require("kanagawa").setup(opts)
+    --   vim.cmd.colorscheme("kanagawa-wave")
+    -- end,
   },
 
   -- tell LazyVim to use this colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa-wave",
+      colorscheme = "kanagawa-dragon",
     },
   },
 }
