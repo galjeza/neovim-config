4afeb99a993251f314060d6a358aa1f1efcbce80
galjeza <gal.jeza55@gmail.com>
2026-01-16 13:45:15 +0100

    remove hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,6 +5,22 @@ return {
       diagnostics = {
         signs = false,
       },
+      servers = {
+        vtsls = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                enumMemberValues = { enabled = false },
+                functionLikeReturnTypes = { enabled = false },
+                parameterNames = { enabled = false },
+                parameterTypes = { enabled = false },
+                propertyDeclarationTypes = { enabled = false },
+                variableTypes = { enabled = false },
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
